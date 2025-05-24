b6620eb7e8852a5920a8471db07a6a3fb73d7fa5
Alvin11David <alvin69david@gmail.com>
2025-05-25 00:24:11 +0300

    finshed the splash screen

--- SPOTIFY/spotify/lib/presentation/splash/pages/splash.dart
+++ SPOTIFY/spotify/lib/presentation/splash/pages/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
