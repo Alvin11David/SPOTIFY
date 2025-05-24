f39e57b001dafb2a38f6ba40a169455963205f3e
Alvin11David <alvin69david@gmail.com>
2025-05-24 23:54:37 +0300

    fixed the fontfam

--- SPOTIFY/spotify/lib/core/configs/theme/app_theme.dart
+++ SPOTIFY/spotify/lib/core/configs/theme/app_theme.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
+
 class AppTheme {
 
   static final lightTheme = ThemeData(
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.lightBackground,
     brightness: Brightness.light,
-    fontFamily: Satoshi,
+    fontFamily: 'Satoshi',
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
@@ -22,7 +23,7 @@ class AppTheme {
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.darkBackground,
     brightness: Brightness.dark,
-    fontFamily: Satoshi,
+    fontFamily: 'Satoshi',
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
