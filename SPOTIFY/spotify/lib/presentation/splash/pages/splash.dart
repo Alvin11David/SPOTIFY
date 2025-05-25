1b040e509c2d60f9eb3456f957833474cd4457c3
Alvin11David <alvin69david@gmail.com>
2025-05-25 09:27:56 +0300

    added the app images file

--- SPOTIFY/spotify/lib/presentation/splash/pages/splash.dart
+++ SPOTIFY/spotify/lib/presentation/splash/pages/splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/presentation/intro/pages/get_started.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -14,6 +15,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
+    redirect();
   }
   @override
   Widget build(BuildContext context) {
@@ -29,8 +31,11 @@ class _SplashPageState extends State<SplashPage> {
   Future<void> redirect() async {
     await Future.delayed(const Duration(seconds: 2));
     Navigator.pushReplacement(
+      // ignore: use_build_context_synchronously
       context, 
-      MaterialPageRoute(builder: (BuildContext context) => const)
+      MaterialPageRoute(
+        builder: (BuildContext context) => const GetStartedPage()
       )
+    );
   }
 }
