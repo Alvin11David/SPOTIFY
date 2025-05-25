8b20248250fc807ed7ebc4978ec3e1f8d85a23b9
Alvin11David <alvin69david@gmail.com>
2025-05-25 10:18:38 +0300

    made the basic app button logic

--- SPOTIFY/spotify/lib/common/widgets/button/basic_app_button.dart
+++ SPOTIFY/spotify/lib/common/widgets/button/basic_app_button.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class BasicAppButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String title;
+  final double ? height;
   const BasicAppButton({
     required this.onPressed,
     required this.title,
+    this.height,
     super.key
     });
 
@@ -13,6 +15,9 @@ class BasicAppButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.fromHeight(height ?? 80)
+      ),
       child: Text(
         title
       )
