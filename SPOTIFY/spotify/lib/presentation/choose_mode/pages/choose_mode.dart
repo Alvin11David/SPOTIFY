c50abab7c7168a97fe9e29a9d5c2d5331a511f18
Alvin11David <alvin69david@gmail.com>
2025-05-25 19:10:14 +0300

    added the sun and moon svgs in the app vectors file

--- SPOTIFY/spotify/lib/presentation/choose_mode/pages/choose_mode.dart
+++ SPOTIFY/spotify/lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class ChooseModePage extends StatelessWidget {
   const ChooseModePage({super.key});
@@ -6,7 +10,109 @@ class ChooseModePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AppImages.chooseModeBG,
+                )
+              )
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: SvgPicture.asset(
+                    AppVectors.logo
+                ),
+              ),
+                Spacer(),
+                Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 21, ),
+                
+                SizedBox(height: 50,),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => const ChooseModePage()
+                        )
+                    );
+                  },
+                  title: 'Get Started',
+                )
+              ],
+            ),
+          ),
+
+          Container(
+            // ignore: deprecated_member_use
+            color: Colors.black.withOpacity(0.15),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      AppVectors.logo
+                  ),
+                ),
+                  Spacer(),
+                  Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 21, ),
+                  Text(
+                    'Discover new music, create playlists, and share your favorite tracks with friends.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                    textAlign: TextAlign.center,
+                  ), 
+                  SizedBox(height: 20,),
+                  BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => const ChooseModePage()
+                          )
+                      );
+                    },
+                    title: 'Get Started',
+                  )
+                ],
+              ),
+          )
+        ],
+      ),
     );
   }
 }
