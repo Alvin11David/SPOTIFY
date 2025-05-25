ca0495bd6f7e7c7838954d8ba0a4b8893f96849d
Alvin11David <alvin69david@gmail.com>
2025-05-25 10:22:20 +0300

    made the basic app button logic

--- SPOTIFY/spotify/lib/presentation/intro/pages/get_started.dart
+++ SPOTIFY/spotify/lib/presentation/intro/pages/get_started.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
@@ -51,6 +52,13 @@ class GetStartedPage extends StatelessWidget {
                   ),
                   textAlign: TextAlign.center,
                 ), 
+                SizedBox(height: 20,),
+                BasicAppButton(
+                  onPressed: () {
+                    
+                  },
+                  title: 'Get Started',
+                )
               ],
             )
           ),
