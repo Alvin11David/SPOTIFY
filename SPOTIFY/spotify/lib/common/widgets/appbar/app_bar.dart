def923f6140950c57c8981541559c53d2f4f03fa
Alvin11David <alvin69david@gmail.com>
2025-05-26 21:43:55 +0300

    made the arrow circular

--- SPOTIFY/spotify/lib/common/widgets/appbar/app_bar.dart
+++ SPOTIFY/spotify/lib/common/widgets/appbar/app_bar.dart
@@ -8,6 +8,16 @@ class BasicAppbar extends StatelessWidget {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
+      leading: IconButton(
+        onPressed: (){},
+        icon: Container(
+          decoration: BoxDecoration(
+            // ignore: deprecated_member_use
+            color: Colors.white.withOpacity(0.03),
+            shape: BoxShape.circle,
+          ),
+        ),
+      ),
     );
   }
 }
