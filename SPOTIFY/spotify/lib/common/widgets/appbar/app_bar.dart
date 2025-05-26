7224f03cafe36251e81dc884d7ed3d72089be490
Alvin11David <alvin69david@gmail.com>
2025-05-26 21:35:44 +0300

    added the basic app bar call

---
+++ SPOTIFY/spotify/lib/common/widgets/appbar/app_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BasicAppbar extends StatelessWidget {
+  const BasicAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      
+    );
+  }
+}
