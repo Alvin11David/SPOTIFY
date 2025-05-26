3c0299b4caa99e1a69ae78b63885c5167eab9adc
Alvin11David <alvin69david@gmail.com>
2025-05-26 21:38:53 +0300

    added the back arrow

--- SPOTIFY/spotify/lib/common/widgets/appbar/app_bar.dart
+++ SPOTIFY/spotify/lib/common/widgets/appbar/app_bar.dart
@@ -6,7 +6,8 @@ class BasicAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      
+      backgroundColor: Colors.transparent,
+      elevation: 0,
     );
   }
 }
