bf1d5c8ef8e5ce69e2e7fc5960ffff00755f9c74
Alvin11David <alvin69david@gmail.com>
2025-05-28 15:32:49 +0300

    changed the signup and text

---
+++ SPOTIFY/spotify/lib/presentation/auth/pages/signin.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart' show SvgPicture;
+import 'package:spotify/common/widgets/appbar/app_bar.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart' show AppVectors;
+
+class SignIpPage extends StatelessWidget {
+  const SignIpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: _signupText(context),
+      appBar: BasicAppbar(
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 30,
+          width: 40,
+        ),
+      ), 
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 50,
+          horizontal: 30
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _registerText(),
+            const SizedBox(height: 40,),
+            _fullNameField(context),
+            const SizedBox(height: 20,),
+            _emailField(context),
+            const SizedBox(height: 20,),
+            _passwordField(context),
+            const SizedBox(height: 40,),
+            BasicAppButton(
+              onPressed: (){}, 
+              title: 'Create Account', 
+              color: Colors.blue, 
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _registerText() {
+    return const Text(
+      'Register',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 25,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _fullNameField(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Full Name',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme
+      ),
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Enter Your Email',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme
+      ),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Enter Your Password',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme
+      ),
+    ); 
+  }
+
+  Widget _signupText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 30, 
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+          'Not A Member?',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 14,
+          ),
+          ),
+          TextButton(
+            onPressed: (){},
+            child: const Text(
+              'Sign Un'
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
