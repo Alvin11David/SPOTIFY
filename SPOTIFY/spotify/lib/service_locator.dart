dfcb1b5081c0bde92ee60cd1e2d677afe70599e9
Alvin11David <alvin69david@gmail.com>
2025-05-29 16:31:30 +0300

    added the logic

---
+++ SPOTIFY/spotify/lib/service_locator.dart
@@ -0,0 +1,13 @@
+
+import 'package:get_it/get_it.dart';
+import 'package:spotify/data/sources/auth/auth_firebase_service.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+
+  sl.registerSingleton<AuthFirebaseService>(
+    AuthFirebaseServicesImpl()
+  );
+
+}
