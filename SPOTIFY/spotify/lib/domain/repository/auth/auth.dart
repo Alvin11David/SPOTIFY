7a5a257cfb338057c43b46e18caf3b269e51d5e7
Alvin11David <alvin69david@gmail.com>
2025-05-30 16:33:57 +0300

    added the logic

--- SPOTIFY/spotify/lib/domain/repository/auth/auth.dart
+++ SPOTIFY/spotify/lib/domain/repository/auth/auth.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart' show Either;
 import 'package:spotify/data/models/auth/create_user_req.dart' show CreateUserReq;
+import 'package:spotify/data/models/auth/signin_user_req.dart' show SigninUserReq;
 
 abstract class AuthRepository {
   Future<Either> signup(CreateUserReq createUserReq);
 
-  Future<void> signin();
+  Future<Either> signin(SigninUserReq signinUserReq);
 }
