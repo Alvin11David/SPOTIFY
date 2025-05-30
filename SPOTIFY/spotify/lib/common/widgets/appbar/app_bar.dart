0db34627f523599e65c14003360fcb668a5426d4
Alvin11David <alvin69david@gmail.com>
2025-05-30 21:42:54 +0300

    added the logic

--- SPOTIFY/spotify/lib/common/widgets/appbar/app_bar.dart
+++ SPOTIFY/spotify/lib/common/widgets/appbar/app_bar.dart
@@ -3,8 +3,10 @@ import 'package:spotify/common/helpers/is_dark_mode.dart';
 
 class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
   final Widget ? title;
+  final bool hideBack;
   const BasicAppbar({
     this.title,
+    this.hideBack = false,
     super.key
     });
 
@@ -15,7 +17,7 @@ class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       centerTitle: true,
       title: title ?? const Text(''),
-      leading: IconButton(
+      leading: hideBack ? null : IconButton(
         onPressed: (){
           Navigator.pop(context);
         },
--- SPOTIFY/spotify/lib/presentation/home/pages/home.dart
+++ SPOTIFY/spotify/lib/presentation/home/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart' show BuildContext, Scaffold, StatelessWidget, Widget;
+import 'package:flutter_svg/svg.dart' show SvgPicture;
+import 'package:spotify/common/widgets/appbar/app_bar.dart' show BasicAppbar;
+import 'package:spotify/core/configs/assets/app_vectors.dart' show AppVectors;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +9,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: BasicAppbar(
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 30,
+          width: 40,
+        ),
+      ),
     );
   }
 }
