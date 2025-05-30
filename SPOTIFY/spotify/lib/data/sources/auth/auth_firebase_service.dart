fef42bcdee250b47435b047a7d7be680eebd907c
Alvin11David <alvin69david@gmail.com>
2025-05-30 16:13:09 +0300

    added the logic

--- SPOTIFY/spotify/lib/data/sources/auth/auth_firebase_service.dart
+++ SPOTIFY/spotify/lib/data/sources/auth/auth_firebase_service.dart
@@ -1,36 +1,36 @@
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spotify/data/models/auth/create_user_req.dart';
+import 'package:spotify/data/models/auth/signin_user_req.dart' show SigninUserReq;
 
 abstract class AuthFirebaseService {
   
-  Future<Either> signup(CreateUserReq CreateUserReq);
-  Future<void> signin();
+  Future<Either> signup(CreateUserReq createUserReq);
+  Future<Either> signin(SigninUserReq signinUserReq);
 
 }
 
 class AuthFirebaseServicesImpl extends AuthFirebaseService {
   @override
-  Future<void> signin() {
+  Future<Either> signin(SigninUserReq signinUserReq) async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: createUserReq.email,
-        password: createUserReq.password
-        );
+        email: signinUserReq.email,
+        password: signinUserReq.password
+      );
 
-        return const Right('SignUp Was Successful');
+      return const Right('SignUp Was Successful');
 
     } on FirebaseAuthException catch (e) {
       String message = '';
 
-      if(e.code == 'weak-password') {
-        message == "The Password Provided is too weak";
-      } else if(e.code == 'email-already-in-use') {
-        message == "An Account already exists with that email.";
+      if (e.code == 'weak-password') {
+        message = "The Password Provided is too weak";
+      } else if (e.code == 'email-already-in-use') {
+        message = "An Account already exists with that email.";
       }
       return Left(message);
     }
-  }
   }
 
   @override
@@ -39,17 +39,17 @@ class AuthFirebaseServicesImpl extends AuthFirebaseService {
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: createUserReq.email,
         password: createUserReq.password
-        );
+      );
 
-        return const Right('SignUp Was Successful');
+      return const Right('SignUp Was Successful');
 
     } on FirebaseAuthException catch (e) {
       String message = '';
 
-      if(e.code == 'weak-password') {
-        message == "The Password Provided is too weak";
-      } else if(e.code == 'email-already-in-use') {
-        message == "An Account already exists with that email.";
+      if (e.code == 'weak-password') {
+        message = "The Password Provided is too weak";
+      } else if (e.code == 'email-already-in-use') {
+        message = "An Account already exists with that email.";
       }
       return Left(message);
     }
