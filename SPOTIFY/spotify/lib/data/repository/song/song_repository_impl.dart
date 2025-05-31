0a2ff6e3b965da1a5b83721c867cadebdb5923cd
Alvin11David <alvin69david@gmail.com>
2025-05-31 13:09:13 +0300

    added the logic

--- SPOTIFY/spotify/lib/data/repository/song/song_repository_impl.dart
+++ SPOTIFY/spotify/lib/data/repository/song/song_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart' show Either;
 import 'package:spotify/data/repository/song/song.dart';
-import 'package:spotify/data/sources/songs/song.dart' show SongFirebaseService;
+import 'package:spotify/data/sources/songs/song_firebase_service.dart' show SongFirebaseService;
 import 'package:spotify/service_locator.dart' show sl;
 
 class SongRepositoryImpl extends SongsRepository {
---
+++ SPOTIFY/spotify/lib/domain/repository/usecases/song/get_news_songs.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart' show Either;
+import 'package:spotify/core/usecase/usecase.dart';
+import 'package:spotify/data/repository/song/song_repository_impl.dart' show SongRepositoryImpl;
+import 'package:spotify/service_locator.dart' show sl;
+
+class GetNewsSongsUseCase implements UseCase<Either,dynamic> {
+  @override
+  Future<Either> call({params}) async {
+    return await sl<SongRepositoryImpl>().getNewsSongs();
+  }
+  
+}
